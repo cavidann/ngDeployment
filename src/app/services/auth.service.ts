df023f4a6b66801c8241e8041220ae9c258b14ed
cavidann <cavi98@icloud.com>
2018-11-29 21:46:13 +0400

    first commit

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { map } from 'rxjs/operators';
+import { tokenNotExpired, JwtHelper } from 'angular2-jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(private http: Http) {
+  }
+
+  login(credentials) {
+   return this.http.post('/api/authenticate',
+      JSON.stringify(credentials))
+      .pipe(
+        map( response => {
+          let result = response.json();
+          if (result && result.token) {
+            localStorage.setItem('token', result.token);
+            return true;
+          }
+          return false;
+        })
+      );
+  }
+
+  public logout() {
+    localStorage.removeItem('token');
+  }
+
+  isLoggedIn() {
+    return tokenNotExpired();
+  }
+
+  get currentUser() {
+    let token = localStorage.getItem('token');
+    if (!token) {
+      return null;
+    }
+
+    return new JwtHelper().decodeToken(token);
+  }
+}
+
